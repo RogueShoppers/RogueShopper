d3ed58f7017a1f0efd768e6a6c87b578f60a8bac
Natalie Rojas <natalie.rojas113@gmail.com>
2021-03-03 15:17:54 -0500

    Updated associations(order-products)

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,9 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  inProcess: {
-    type: Sequelize.BOOLEAN
-  },
   completed: {
     type: Sequelize.BOOLEAN
   }
