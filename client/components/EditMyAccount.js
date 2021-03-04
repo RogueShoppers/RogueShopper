5ac7ac8c9be6d56462b778cd439462f9fdd5ae50
Heather Ha <hyunjung222@gmail.com>
2021-03-03 22:54:43 -0500

    MyAccount shows logged-in user profile & placeholder for order history & allows edit profile

---
+++ client/components/EditMyAccount.js
@@ -0,0 +1,157 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {editMe} from '../store/user'
+import {Link} from 'react-router-dom'
+
+class EditMyAccount extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+      preferredName: '',
+      address: '',
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      firstName: this.props.user.firstName,
+      lastName: this.props.user.lastName,
+      preferredName: this.props.user.preferredName,
+      address: this.props.user.address,
+      email: this.props.user.email
+      //   password: this.props.user.password,
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.editUser(this.state)
+  }
+
+  render() {
+    const {
+      firstName,
+      lastName,
+      preferredName,
+      address,
+      email,
+      password
+    } = this.state
+
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <div className="input-field">
+            <label htmlFor="firstName" className="active">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="lastName" className="active">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="preferredName" className="active">
+              Preferred Name
+            </label>
+            <input
+              type="text"
+              id="preferredName"
+              value={preferredName}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="address" className="active">
+              Address
+            </label>
+            <input
+              type="text"
+              id="address"
+              value={address}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="email" className="active">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="password" className="active">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <button type="submit" className="btn blue lighten-1">
+              Update
+            </button>
+
+            <Link to="/me">
+              <button type="submit" className="btn pink lighten-2">
+                Cancel
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.users.selected
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    editUser: user => dispatch(editMe(user))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditMyAccount)
--- client/components/MyUserAccount.js
+++ client/components/MyUserAccount.js
@@ -1,16 +1,67 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getMe} from '../store/user'
+import {Link} from 'react-router-dom'
 
 class MyUserAccount extends Component {
   componentDidMount() {
-    console.log('componentDidMount')
     this.props.getMe()
   }
   render() {
-    console.log('inside render()')
-    const loggedInUser = this.props.user
-    return loggedInUser.email
+    const {
+      firstName,
+      lastName,
+      preferredName,
+      address,
+      email,
+      password
+    } = this.props.user
+
+    return (
+      <div className="container">
+        <h2>Hi {!preferredName ? firstName : preferredName}!</h2>
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">Profile</span>
+                <div>
+                  <p>First Name: {firstName}</p>
+                  <p>Last Name: {lastName}</p>
+                  <p>Preferred Name: {preferredName}</p>
+                  <p>Address: {address}</p>
+                  <p>Email: {email}</p>
+                  <p>Password: {password}</p>
+                </div>
+              </div>
+              <div className="card-action">
+                <Link to="/me/edit">Edit</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="divider" />
+        <h5>Order History</h5>
+        <table className="highlight">
+          <thead>
+            <tr>
+              <th>Order Number</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>[Placeholder]</td>
+              <td>[Placeholder]</td>
+            </tr>
+            <tr>
+              <td>[Placeholder]</td>
+              <td>[Placeholder]</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
