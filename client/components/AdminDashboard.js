ced473b39b7944f33c561189176715dd6835702d
kristen <studiosok@gmail.com>
2021-03-07 11:13:11 -0500

    admin dashboard rework

--- client/components/AdminDashboard.js
+++ client/components/AdminDashboard.js
@@ -43,105 +43,10 @@ class AdminDashboard extends Component {
   }
 
   render() {
+    //have admin user choose products or users dashboard
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <div className="input-field">
-            <label htmlFor="firstName" className="active">
-              First Name
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              value={firstName}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="input-field">
-            <label htmlFor="lastName" className="active">
-              Last Name
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              value={lastName}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="input-field">
-            <label htmlFor="preferredName" className="active">
-              Preferred Name
-            </label>
-            <input
-              type="text"
-              id="preferredName"
-              value={preferredName}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="input-field">
-            <label htmlFor="address" className="active">
-              Address
-            </label>
-            <input
-              type="text"
-              id="address"
-              value={address}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="input-field">
-            <label htmlFor="email" className="active">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="input-field">
-            <label htmlFor="password" className="active">
-              Password
-            </label>
-            <input
-              placeholder="******"
-              type="password"
-              id="password"
-              defaultValue={password}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="input-field">
-            <label htmlFor="isAdmin" className="active">
-              Admin Status
-            </label>
-            <input
-              type="text"
-              id="isAdmin"
-              defaultValue={String(isAdmin)}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div>
-            <button type="submit" className="btn blue lighten-1">
-              Update
-            </button>
-
-            <Link to="/me">
-              <button type="submit" className="btn pink lighten-2">
-                Cancel
-              </button>
-            </Link>
-          </div>
-        </form>
+        <div id="adminSearch" />
       </div>
     )
   }
