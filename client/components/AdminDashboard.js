5f5c764e9b99e3942a7b8171cbeec93aa9367710
kristen <studiosok@gmail.com>
2021-03-07 10:58:24 -0500

    debug in process - admin dashboard

---
+++ client/components/AdminDashboard.js
@@ -0,0 +1,162 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {editMe} from '../store/user'
+
+class AdminDashboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+      preferredName: '',
+      address: '',
+      email: '',
+      password: '',
+      isAdmin: false
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      firstName: this.props.user.firstName,
+      lastName: this.props.user.lastName,
+      preferredName: this.props.user.preferredName,
+      address: this.props.user.address,
+      email: this.props.user.email,
+      password: this.props.user.password,
+      isAdmin: this.props.user.isAdmin
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.editUser(this.state)
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <div className="input-field">
+            <label htmlFor="firstName" className="active">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="lastName" className="active">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="preferredName" className="active">
+              Preferred Name
+            </label>
+            <input
+              type="text"
+              id="preferredName"
+              value={preferredName}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="address" className="active">
+              Address
+            </label>
+            <input
+              type="text"
+              id="address"
+              value={address}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="email" className="active">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-field">
+            <label htmlFor="password" className="active">
+              Password
+            </label>
+            <input
+              placeholder="******"
+              type="password"
+              id="password"
+              defaultValue={password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="isAdmin" className="active">
+              Admin Status
+            </label>
+            <input
+              type="text"
+              id="isAdmin"
+              defaultValue={String(isAdmin)}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <button type="submit" className="btn blue lighten-1">
+              Update
+            </button>
+
+            <Link to="/me">
+              <button type="submit" className="btn pink lighten-2">
+                Cancel
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    editMe: user => dispatch(editMe(user))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminDashboard)
