fa955f19e6f86f883d5ff38d18ab2dc567dfdcad
akmr328 <akm328@gmail.com>
2021-03-08 16:55:33 -0500

    implemented materialize pagination, working on js

--- client/components/allproducts.js
+++ client/components/allproducts.js
@@ -40,6 +40,33 @@ const AllProducts = props => {
             : 'No Products on Database'}
         </div>
       </div>
+      <ul className="center pagination">
+        <li className="disabled">
+          <a href="#!">
+            <i className="material-icons">chevron_left</i>
+          </a>
+        </li>
+        <li className="active">
+          <a href="#!">1</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">2</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">3</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">4</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">5</a>
+        </li>
+        <li className="waves-effect">
+          <a href="#!">
+            <i className="material-icons">chevron_right</i>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
---
+++ client/components/pagination.js
@@ -0,0 +1,19 @@
+// import React, { useState, useEffect } from 'react';
+
+// const Pagination = props => {
+//   const [data, setData] = useState([])
+
+//   useEffect(() => {
+
+//   }, [])
+
+//   handleClick = (event) => {
+//     setCurrentPage(Number(event.target.id))
+//   };
+
+//     return(
+//       <div>
+
+//     </div>
+//   )
+// }
