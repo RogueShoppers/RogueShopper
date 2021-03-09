a5106b4c0bc6fad8e23c627a21c855955c6598ae
Heather Ha <hyunjung222@gmail.com>
2021-03-09 13:07:14 -0500

    Working in progress

--- client/components/FilterProducts.js
+++ client/components/FilterProducts.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {filterProducts} from '../store/products'
 
-const FilterProducts = props => {
+const FilterProduct = props => {
   const {products} = props
 
-  console.log('products inside FilterProducts--->', products)
+  console.log('props inside FilterProducts--->', props)
 
   return (
     <div>
-      <h6>Filter by Category</h6>
-      <select>
+      <label>Filter by Category</label>
+      <select
+        className="browser-default"
+        value={props.toyType}
+        onChange={e => props.filterProducts(props.products, e.target.value)}
+      >
         <option value="">All</option>
+        <option value="chase">Chase</option>
+        <option value="chew">Chew</option>
+        <option value="destroy">Destroy</option>
+        <option value="brain teaser">Brain Teaser</option>
+        <option value="tug">Tug</option>
+        <option value="fetch">Fetch</option>
+        <option value="feed">Feed</option>
+        <option value="squishy">Squishy</option>
       </select>
     </div>
   )
@@ -18,8 +31,16 @@ const FilterProducts = props => {
 
 const mapStateToProps = state => {
   return {
-    products: state.products.all
+    toyType: state.products.toyType,
+    filteredProducts: state.products.filteredItems
+    // filtered: state.products.filtered,
   }
 }
 
-export default connect(mapStateToProps)(FilterProducts)
+const mapDispatchToProps = dispatch => {
+  return {
+    filterProducts: () => dispatch(filterProducts())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilterProduct)
--- client/store/products.js
+++ client/store/products.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 const SET_PRODUCTS = 'SET_PRODUCTS'
 const SET_SINGLE_PRODUCT = 'SET_SINGLE_PRODUCT'
 const FILTER_PRODUCTS = 'FILTER_PRODUCTS'
+// const ADD_FILTER = 'ADD_FILTER'
 
 //ACTION CREATOR
 export const setProducts = products => ({
@@ -14,16 +15,28 @@ export const setSingleProduct = product => ({
   type: SET_SINGLE_PRODUCT,
   product
 })
-export const filteredAllProducts = (products, toyType) => ({
-  type: FILTER_PRODUCTS,
-  payload: {
-    toyType,
-    filteredProducts:
-      toyType === ''
-        ? products
-        : products.filter(product => product.type === toyType)
-  }
-})
+// export const filteredProducts = (filteredProduct) => ({
+//   type: FILTER_PRODUCTS,
+//   filteredProduct,
+// })
+
+// export const addFilter = (filter) => ({
+//   type: ADD_FILTER,
+//   filter,
+// })
+
+export const filterProducts = (products, toyType) => dispatch => {
+  dispatch({
+    type: FILTER_PRODUCTS,
+    payload: {
+      toyType,
+      items:
+        toyType === ''
+          ? products
+          : products.filter(product => product.tags.indexOf(toyType) >= 0)
+    }
+  })
+}
 
 //THUNKS
 export const fetchAllProducts = () => {
@@ -46,16 +59,32 @@ export const fetchSingleProduct = id => {
     }
   }
 }
-// export const filterAllProducts = (products, toyType) => {
-//   return dispatch => {
-//     payload: {
-//       toyType,
-//       filteredProducts:
-//         toyType === ''
-//           ? products
-//           : products.filter(product => product.type === toyType)
+// export const filterProducts = (toyType) => {
+//   return async (dispatch) => {
+//     try {
+//       const {data: items} = await axios.get('/api/products')
+//       console.log('items inside filterProducts-->', items)
+//       if(toyType === '')
+//       // const filteredItems =
+//       // toyType === ''
+//       //   ? items
+//       //   : items.filter((item) => item.tags.indexOf(toyType) >= 0)
+//       dispatch(
+//         filteredProducts(
+//           toyType === ''
+//             ? items
+//             : items.filter((item) => item.tags.indexOf(toyType) >= 0)
+//         )
+//       )
+//     } catch (error) {
+//       console.log('Error: Could not get filtered products', error)
 //     }
-//     dispatch(filteredAllProducts())
+//   }
+// }
+
+// export const addFilters = (filter) => {
+//   return (dispatch) => {
+//     dispatch(addFilter(filter))
 //   }
 // }
 
@@ -63,6 +92,7 @@ export const fetchSingleProduct = id => {
 const initialState = {
   all: [],
   selected: {}
+  // filtered: [],
 }
 
 //REDUCER
@@ -72,11 +102,24 @@ export default function productsReducer(state = initialState, action) {
       return {...state, all: action.products}
     case SET_SINGLE_PRODUCT:
       return {...state, selected: action.product}
+    // case FILTER_PRODUCTS:
+    //   return {
+    //     ...state,
+    //     filtered: action.filteredProduct,
+    //   }
+    // case ADD_FILTER:
+    //   return {
+    //     ...state,
+    //     filtered:
+    //       action.filter === ''
+    //         ? state.all
+    //         : state.all.filter((item) => item.tags.indexOf(action.filter) >= 0),
+    //   }
     case FILTER_PRODUCTS:
       return {
         ...state,
         toyType: action.payload.toyType,
-        filteredProducts: action.payload.filteredProducts
+        filteredItems: action.payload.items
       }
     default:
       return state
